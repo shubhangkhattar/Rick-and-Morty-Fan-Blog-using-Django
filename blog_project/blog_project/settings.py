0ef88d3875d4fabf44020d0b220c30bae1b6e01c
shubhang khattar <shubhang1995khattar@gmail.com>
2023-10-28 18:16:33 -0400

    Add requirements.txt

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shubhang_db',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'discodancer',
     }
 }
 
