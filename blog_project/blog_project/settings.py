9065ba000947af01cc95128d96d07325311c8662
shubhang khattar <shubhang1995khattar@gmail.com>
2023-10-28 20:36:53 -0400

    mysql to sqllit

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -88,15 +88,21 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'shubhang_db',
+#         'USER': 'root',
+#         'PASSWORD': 'discodancer',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shubhang_db',
-        'USER': 'root',
-        'PASSWORD': 'discodancer',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
